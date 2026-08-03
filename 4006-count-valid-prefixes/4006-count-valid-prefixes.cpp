ba565731cd6076ab0c1a7cb449d05a79ca39e8c6
Arpit Singh <rais41902@gmail.com>
2026-08-03 21:39:52 +0530

    Time: 0 ms (100%), Space: 9.4 MB (66.67%) - LeetHub

---
+++ 4006-count-valid-prefixes/4006-count-valid-prefixes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countValidPrefixes(string s) {
+        int cnt0=0;
+        int cnt1=0;
+        int ans=0;
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            if(s[i]=='0') cnt0++;
+            else cnt1++;
+            if(abs(cnt0-cnt1)<=1){
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
